e7ab88a7a090b84814c258d63d2adb143a877117
sighmon <simon.loffler@gmail.com>
2016-07-10 14:01:37 +0930

    Added config for action_cable so that other users can connect to my web socket.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Bikelab
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Allow all action_cable requests
+    # config.action_cable.disable_request_forgery_protection = true
+
+    # Allow all requests from me
+    config.action_cable.allowed_request_origins = ['http://10.0.1.91:3000', 'http://localhost:3000']
   end
 end
