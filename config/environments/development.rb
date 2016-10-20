72329a0b1af0e7c1a05a732145cbf33b8e01660b
sighmon <simon.loffler@gmail.com>
2016-10-20 17:09:43 +1030

    Added allowed_request_origins set to all for local development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # For testing across local networks allow all request origins
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 end
